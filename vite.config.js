18b2e1c0fda00be766b036b3233ee3056bf9d0cb
Yakov Lemenkov <101573780+JaZhKa@users.noreply.github.com>
2025-10-23 14:31:28 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/experiment/',
+  esbuild: {
+    include: /\.(jsx|tsx?)$/,
+    loader: 'jsx',
+    jsx: 'automatic',
+  },
 });
